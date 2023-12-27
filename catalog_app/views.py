5cdac17c2351efcca0b00df239c469525263fc48
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-27 12:37:27 +0300

    paginator

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -3,15 +3,11 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Product, Category
 
 
-def catalog(request, slug, page=1):
-    if slug == 'all':
-        goods = Product.objects.all()
-    else:
-        goods = get_list_or_404(Product.objects.filter(category__slug=slug))
-
+def catalog(request, slug):
+    page = int(request.GET.get('page', 1))
+    goods = Product.objects.all() if slug == 'all' else get_list_or_404(Product.objects.filter(category__slug=slug))
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
-
     context = {
         'title': 'FurnitureShop - Catalog',
         'goods': current_page,
