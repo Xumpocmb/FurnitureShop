b7e8bf32274650082714f98792968f3a69bdd130
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-27 13:33:19 +0300

    filters & url encode

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -5,7 +5,16 @@ from .models import Product, Category
 
 def catalog(request, slug):
     page = int(request.GET.get('page', 1))
+    on_sale = request.GET.get('on_sale', None)
+    ordering = request.GET.get('order_by', None)
+
     goods = Product.objects.all() if slug == 'all' else get_list_or_404(Product.objects.filter(category__slug=slug))
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if ordering and ordering != 'default':
+        goods = goods.order_by(ordering)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
     context = {
