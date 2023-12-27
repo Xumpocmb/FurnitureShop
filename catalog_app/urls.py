6d5fe0c6724628ca9639b5e0fee2004533e60e95
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-27 23:22:15 +0300

    search

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -5,6 +5,7 @@ from .views import catalog, product
 app_name = 'catalog_app'
 
 urlpatterns = [
+    path('search/', catalog, name='search'),
     path('<slug:slug>/', catalog, name='catalog'),
     path('product/<slug:slug>/', product, name='product'),
 ]
--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,14 +1,23 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import Product, Category
+from django.shortcuts import render, get_list_or_404
+
+from .models import Product
+from .utils import q_search
 
 
-def catalog(request, slug):
+def catalog(request, slug=None):
     page = int(request.GET.get('page', 1))
     on_sale = request.GET.get('on_sale', None)
     ordering = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
-    goods = Product.objects.all() if slug == 'all' else get_list_or_404(Product.objects.filter(category__slug=slug))
+    if slug == 'all':
+        goods = Product.objects.all()
+    elif query:
+        goods = q_search(query)
+    else:
+        goods = get_list_or_404(Product.objects.filter(category__slug=slug))
+
     if on_sale:
         goods = goods.filter(discount__gt=0)
 
