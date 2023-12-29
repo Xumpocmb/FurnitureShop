46e4cae0fc2e8cc7bffc30106b5d7288a8eeffc9
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-29 13:55:01 +0300

    add to cart without reload

--- carts_app/views.py
+++ carts_app/views.py
@@ -1,19 +1,31 @@
 from django.db.models import F
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from carts_app.models import Cart
+from carts_app.utils import get_user_carts
 from catalog_app.models import Product
 
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_add(request):
+    product = Product.objects.get(id=request.POST.get('product_id'))
     if request.user.is_authenticated:
         cart = Cart.objects.filter(user=request.user, product=product)
         if cart.exists():
             cart.update(quantity=F('quantity') + 1)
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META.get('HTTP_REFERER'))
+    user_carts = get_user_carts(request)
+    cart_items_html = render_to_string('cart_modal.html',
+                                       {'carts': user_carts},
+                                       request=request)
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
